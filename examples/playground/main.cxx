595e5c9ea5e9f11cc665a66fc43159696f080051
Chris Nichols <chris@chrisnichols.ca>
2026-06-01 23:23:44 -0600

    Updated example code using new std::format support

--- examples/playground/main.cxx
+++ examples/playground/main.cxx
@@ -1,12 +1,28 @@
-#include <iostream>
-
 import cns;
 
-auto main() -> int {
-    const auto v1 = cns::Vector3D{1.0, 2.0, 3.0};
-    const auto v2 = cns::Vector3D{-3.0, 2.0, -1.0};
+#include <print>
 
-    std::cout << cns::magnitude(v1 - v2) << "\n";
+auto main() -> int {
+
+    try {
+        const auto v1 = cns::Vector3D{1.0, 2.0, 3.0};
+        const auto v2 = cns::Vector3D{-3.0, 2.0, -1.0};
+
+        std::println("Magnitude of v1 - v2 = {}", cns::magnitude(v1 - v2));
+
+        const auto a = cns::Vector3D{1.0, 2.0, 3.0};
+        const auto b = cns::Vector3D{-3.0, 7.0, -1.0};
+        const auto c = cns::Vector3D{5.0, -17.0, -10.0};
+
+        const auto a1 = cns::project(a, b) + cns::reject(a, b);
+        std::println("a1 = {}", a1);
+        const auto b1 = cns::project(b, c) + cns::reject(b, c);
+        std::println("b1 = {}", b1);
+        const auto c1 = cns::project(c, a) + cns::reject(c, a);
+        std::println("c1 = {}", c1);
+    } catch (...) {
+        return -1;
+    }
 
     return 0;
 }
